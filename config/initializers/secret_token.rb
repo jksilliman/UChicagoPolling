854648312b501e36d15bb7526e18a17581f56b7f
Jesse Silliman <jksilliman@gmail.com>
2011-11-01 20:53:57 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Otis::Application.config.secret_token = 'ce220a8d573d80d8d57fe87052639f27c4969775ae90e22dc380f30ba2a28b96c14e9708eb083e89219e5e7e160c04881f9a60ea016e0ceae68853030b72e726'
