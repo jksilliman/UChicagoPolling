c52d92aa9be611d25e263b22118578665d6f5bed
Jesse Silliman <jksilliman@gmail.com>
2012-04-22 23:11:23 -0500

    Changed deploy

--- config/deploy2.rb
+++ config/deploy2.rb
@@ -1,22 +1,21 @@
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"    
-set :rvm_ruby_string, '1.9.3' # you probably have this already
-set :rvm_type, :system # this is the money config, it defaults to :system
+set :rvm_ruby_string, '1.9.2' # you probably have this already
+set :rvm_type, :user # this is the money config, it defaults to :system
 
 
 set :bundle_roles, :app
 require 'bundler/capistrano'
 default_run_options[:pty] = true
 
-set :application, "polling"
-set :repository,  "git://github.com/jksilliman/UChicagoPolling.git"
+set :application, "otis"
+set :repository,  "git://github.com/jksilliman/Otis.git"
 set :scm, :git
 set :deploy_via, :remote_cache
-set :deploy_to, "/var/www/polling"
+set :deploy_to, "/var/www/otis"
 
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-server "delphi.uchicago.edu:61527", :app, :web, :db, :primary => true
-set :user, "delphi"
+server "sg.uchicago.edu", :app, :web, :db, :primary => true
+set :user, "jksilliman"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
