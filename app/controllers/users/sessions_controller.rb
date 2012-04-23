f2eaa3dfe7018d72c7e021bc27f55e0239f39005
Jesse Silliman <jksilliman@gmail.com>
2012-04-23 00:16:15 -0500

    Fixing login?

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,14 @@
 class Users::SessionsController < Devise::SessionsController
 
+  def create
+    resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")
+    puts resource
+    puts resource.model_name
+    set_flash_message(:notice, :signed_in) if is_navigational_format?
+    sign_in(resource_name, resource)
+    respond_with resource, :location => redirect_location(resource_name, resource)
+  end
+
   def after_sign_in_path_for(resource)
     if session[:url_back]
       temp = session[:url_back]
